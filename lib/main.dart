56c23d7fd49d3c02b623717d5e78a92e72baa989
Thinhskyduck <160042245+Thinhskyduck@users.noreply.github.com>
2025-11-29 19:11:44 +0700

    Remove Firebase configuration files and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'features/1_home/screens/home_screen.dart'; // Import Home
-import 'firebase_options.dart';
+
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
